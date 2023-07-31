4e3616d48e6f06d7469eb2a31ea8dbca4416129a
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2023-08-01 04:09:06 +0900

    Fixed

---
+++ src/pages/api/nft/collection/index.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+
+const apiUrl = process.env.API_URL
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  try {
+    const response = await axios.get(`${apiUrl}/api/nft/collection`)
+
+    res.status(200).json({
+      data: response.data.data,
+    })
+  } catch (error: any) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+}
